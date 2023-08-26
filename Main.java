9c24f4111e1339281945a686828c0036ac5ebda4
UzielMarquez <142339351+UzielMarquez@users.noreply.github.com>
2023-08-26 15:33:58 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,46 +1,96 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
+import java.io.*;
 
 public class Main{
     public static void main(String[] args) {
 
-        String ubicacion = "C:\\Users\\Uziel\\OneDrive\\Escritorio\\ArchivosTareas\\divina_comedia_sp.txt";
-        HashMap<Integer, Integer> histograma = new HashMap<>();
+        try {
 
-        try{
+            FileInputStream fileInputStreamMa = new FileInputStream("C:\\Users\\Uziel\\Downloads\\a.mat"); // Abre el flujo
+            int filasMa = fileInputStreamMa.read(); // lee las filas matriz a.mat
+            int columnasMa = fileInputStreamMa.read(); // lee las columnas b.mat
+            double[][] matriz_amat = datosMatriz(fileInputStreamMa, filasMa, columnasMa); // acomoda los datos de la matriz a.mat
+            fileInputStreamMa.close(); // cierra el flujo
 
-            BufferedReader reader = new BufferedReader(new FileReader(ubicacion));
+            FileInputStream fileInputStreamMb = new FileInputStream("C:\\Users\\Uziel\\OneDrive\\Escritorio\\ArchivosTareas\\b.mat"); // abre el flujo
+            int filasMb = fileInputStreamMb.read(); // lee las filas matriz b.mat
+            int columnasMb = fileInputStreamMb.read(); // lee columnas b.mat
+            double[][] matriz_bmat = datosMatriz(fileInputStreamMb, filasMb, columnasMb);
+            fileInputStreamMb.close(); // cierra el flujo
 
-            String linea;
+            double[][] productoMatrices = multiplicarMatrices(matriz_amat, matriz_bmat); // acomoda los datos del producto de la matriz b.mat y b.mat
 
-            while ((linea = reader.readLine()) != null) {
-                String[] palabras = linea.split("\\s=");
+            FileOutputStream archivoSalida = new FileOutputStream("c.mat"); // Nombre del archivo de salida de la matriz c.mat
+            archivoSalida.write(productoMatrices.length);  // escribe el resultado del producto
+            archivoSalida.write(productoMatrices[0].length);
+            resultMatrizC(archivoSalida, productoMatrices);
+            archivoSalida.close(); // cierra el flujo del archivo de salida
 
-                for (String palabra : palabras) {
-                    palabra = palabra.replaceAll("[^a-zA-Z]", "");
-                    int longitud = palabra.length();
 
-                    if(longitud >= 2 && longitud <= 10){
+            FileInputStream fileInputStreamMc = new FileInputStream("c.mat"); // abre el flujo con el archivo c.mat
+            int filasMc = fileInputStreamMc.read(); // entero que almacena los datos de las filas para c.mat
+            int columnasMc = fileInputStreamMc.read(); // entero que almacena los datos de las columnas para c.mat
+            double[][] matriz_cmat = datosMatriz(fileInputStreamMc, filasMc, columnasMc); // arreglo que crea la matriz
+            fileInputStreamMc.close();
 
-                        histograma.put(longitud, histograma.getOrDefault(longitud, 0)+1);
+            //Imprimir matrices
 
-                    }
+            System.out.println("Matriz a (a.mat): ");
+            imprimirMatrices(matriz_amat);
+
+            System.out.println("Matriz b (b.mat): ");
+            imprimirMatrices(matriz_bmat);
+
+            System.out.println("Matriz c = ((Matriz a)(Matriz b)): ");
+            imprimirMatrices(matriz_cmat);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Metodos
+    private static double[][] multiplicarMatrices(double[][] A, double[][] B) {
+        int rowsA = A.length;
+        int colsA = A[0].length;
+        int colsB = B[0].length;
+
+        double[][] result = new double[rowsA][colsB];
+
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                for (int k = 0; k < colsA; k++) {
+                    result[i][j] += A[i][k] * B[k][j];
                 }
             }
-
-            reader.close();
-
-            for (int longitud = 2; longitud <= 10; longitud++){
-                int freciencia = histograma.getOrDefault(longitud, 0);
-                System.out.printf("palabras con "+"%d caracteres: %s%n", longitud, "▉".repeat(freciencia) );
-            }
-
-        }catch (IOException e){
-
-            System.out.println("No fue posible leer el archivo: " + e.getMessage());
-
         }
+        return result;
+    }
+    private static void resultMatrizC(OutputStream stream, double[][] matrix) throws IOException {
+        DataOutputStream dataStream = new DataOutputStream(stream);
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                dataStream.writeDouble(matrix[i][j]);
+            }
+        }
+    }
+    private static void imprimirMatrices(double[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+    private static double[][] datosMatriz(InputStream stream, int rows, int cols) throws IOException {
+        double[][] matrix = new double[rows][cols];
+        DataInputStream dataStream = new DataInputStream(stream);
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = dataStream.readDouble();
+            }
+        }
+        return matrix;
     }
 }
